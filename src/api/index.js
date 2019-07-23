308b32ffdb92ba9efb4b582e6226bb2b672f6e95
ws <942590550@qq.com>
2019-07-23 09:32:45 +0800

    添加路由

--- src/api/index.js
+++ src/api/index.js
@@ -3,15 +3,18 @@ import _ajax from './_ajax'
 // 基础路径
 // const BASE_URL = 'http://127.0.0.1:3000';
 
-// 请求首页轮播图
+// 请求 首页 轮播图
 export const getHomeCarousel = ()=>_ajax('/api/homecarousel');
 
-// 请求首页导航
+// 请求 首页 导航
 export const getHomeNav = ()=>_ajax('/api/homenav');
 
-// 请求首页商品列表
+// 请求 首页 商品列表
 export const getHomeShopList = ()=>_ajax('/api/homeshoplist');
 
-// 请求推荐页商品列表
+// 请求 推荐 页商品列表
 export const getRecommendShopList = ()=>_ajax('/api/recommendshoplist');
+
+// 请求 搜索 页数据
+export const getSearchGoods = ()=>_ajax('/api/searchgoods');
 
